fix variance

datasets/base_dataset.py
@@ -14,8 +14,9 @@ class BaseDataset(object, metaclass=ABCMeta):
                   'val': [], 'test': []}
     TRANSFORM = {'train': Compose([]), 'train_full': Compose([]), 'train_u': Compose([]),
                  'val': Compose([]), 'test': Compose([])}
-    
+    SUBSET = 10000
     ORI_SIZE = 0
+    U_SELECTED = []
 
     def __init__(self,
                  data_path=None,
@@ -75,11 +76,12 @@ class BaseDataset(object, metaclass=ABCMeta):
         self.QUERIED_HISTORY.append(initial_lb.tolist())
 
     def select_ulb(self):
-        U_SELECTED = np.arange(len(self.DATA_INFOS['train_full']))[~self.INDEX_LB]
-#         if self.SUBSET >= len(self.DATA_INFOS['train_full']) - len(self.DATA_INFOS['train']):
-#             U_SELECTED = U_TEMP
-#         else:
-#             U_SELECTED = np.random.choice(U_TEMP, self.SUBSET, replace=False)
+        U_TEMP = np.arange(len(self.DATA_INFOS['train_full']))[~self.INDEX_LB]
+        if self.SUBSET >= len(self.DATA_INFOS['train_full']) - len(self.DATA_INFOS['train']):
+            U_SELECTED = U_TEMP
+        else:
+            U_SELECTED = np.random.choice(U_TEMP, self.SUBSET, replace=False)
+        self.U_SELECTED = U_SELECTED
         self.INDEX_ULB = np.array([True if x in U_SELECTED else False
                                    for x in range(len(self.DATA_INFOS['train_full']))])
         self.DATA_INFOS['train_u'] = np.array(self.DATA_INFOS['train_full'])[self.INDEX_ULB].tolist()

query_strategies/score_strategy.py
@@ -2,6 +2,7 @@ import torch
 from .strategy import Strategy
 from .builder import STRATEGIES
 from datasets.dataloader import GetHandler
+import numpy as np
 
 
 @STRATEGIES.register_module()
@@ -48,17 +49,29 @@ class ScoreStrategy(Strategy):
         if self.args.aug_metric_ulb == 'density':
             self.calculating_sim_matrix(dataset_u)
             
+#         print(len(dataset_u))
+#         print(len(self.dataset.U_SELECTED))
+#         print(len(self.dataset.DATA_INFOS['train_u']))
+#         print(self.dataset.U_SELECTED)
+            
         
         aggre_scores = self.aggregate_scores(self.calculating_scores(dataset_u)).cpu()
-        score = aggre_scores.numpy()
-        for i in range(len(self.dataset.INDEX_ULB)):
-            if (self.dataset.INDEX_ULB[i] == False):
-                score = np.insert(score, i, 0, axis=None)
+        mean = torch.mean(aggre_scores)
+        score = np.zeros(len(self.dataset.INDEX_ULB))
+        score[self.dataset.U_SELECTED] = aggre_scores
         self.prev_score.append(score)
-        if (len(self.prev_score) > 1):
-            score = torch.tensor(self.prev_score)
-            variance = torch.var(score, dim = 0)
-            final_score = aggre_scores + 0.1 * variance[self.dataset.INDEX_ULB]
-            return final_score.sort()[1][-n:]
-        else:
-            return aggre_scores.sort()[1][-n:]
+        temp = []
+        variance = []
+        for i in self.prev_score:
+            i = i[self.dataset.U_SELECTED]
+            temp.append(i)
+        temp = torch.tensor(np.array(temp))
+        temp = temp.T
+        for i in temp:
+            if (len(i[i != 0]) > 1):
+                variance.append(torch.var(i[i != 0]))
+            else:
+                variance.append(mean)
+        variance = torch.tensor(np.array(variance))
+        aggre_scores += variance
+        return aggre_scores.sort()[1][-n:]